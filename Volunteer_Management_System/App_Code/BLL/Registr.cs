52f416f43ef85ecacc66fb4ed99650cd12d96960
הרשי <y6764057@gmail.com>
2024-06-17 01:39:08 +0300

    ADD fileds

---
+++ Volunteer_Management_System/App_Code/BLL/Registr.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BLL
+{
+    public class Registr
+    {
+        public int RegistrID { get; set; }
+        public int UserID { get; set; }
+        public int ActivitieID { get; set; }
+        public DateTime RegistrData { get; set; }
+        public int Status { get; set; }
+    }
+}
