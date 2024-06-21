da2ef86c5e01e297bcd3fe906d8bf301b84795c3
הרשי <y6764057@gmail.com>
2024-06-21 11:11:51 +0300

    add crud

--- Volunteer_Management_System/App_Code/DAL/ActivitieDAL.cs
+++ Volunteer_Management_System/App_Code/DAL/ActivitieDAL.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using BLL;
+using Data;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +10,93 @@ namespace DAL
 {
     public class ActivitieDAL
     {
+        public static void Save(Activitie tmp)
+        {
+
+            string Sql = "";
+            if (tmp.ActivitieID == -1)
+            {
+                Sql = $"insert into T_Activitie (ActivityName, ADescription, City, GuarantorID, MinParticipants, MaxParticipants, SpecialDemands, Category)";
+                Sql += $" values (N'{tmp.ActivityName}',{tmp.ADescription},{tmp.City},{tmp.GuarantorID},{tmp.MinParticipants},{tmp.MaxParticipants},{tmp.SpecialDemands},{tmp.Category} ";
+            }
+            else
+            {
+                Sql += "update T_Activitie set ";
+                Sql += $" ActivityName= N'{tmp.ActivityName}' ";
+                Sql += $" ADescription= {tmp.ADescription} ";
+                Sql += $" City= {tmp.City} ";
+                Sql += $" GuarantorID= {tmp.GuarantorID} ";
+                Sql += $" MinParticipants= {tmp.MinParticipants} ";
+                Sql += $" MaxParticipants= {tmp.MaxParticipants} ";
+                Sql += $" SpecialDemands= {tmp.SpecialDemands} ";
+                Sql += $" Category= {tmp.Category} ";
+                Sql += $" where ActivitieID = {tmp.ActivitieID} ";
+            }
+            DBContext DB = new DBContext();
+            DB.ExecuteNonQuery(Sql);
+            if (tmp.ActivitieID == -1)
+            {
+                Sql = $"select max(ActivitieID) from T_Activitie where ActivityName= N'{tmp.ActivityName}' ";
+                tmp.ActivitieID = (int)DB.ExecuteScalar(Sql);
+                DB.Close();
+            }
+        }
+        public static List<Activitie> GetAll()
+        {
+            List<Activitie> LstActivitie = new List<Activitie>();
+            string Sql = "select * from T_Activitie";
+            DBContext DB = new DBContext();
+            DataTable DT = DB.Execute(Sql);
+            for (int i = 0; i < DT.Rows.Count; i++)
+            {
+                LstActivitie.Add(new Activitie()
+                {
+                    ActivitieID = (int)DT.Rows[i]["ActivitieID"],
+                    ActivityName = DT.Rows[i]["ActivityName"]+"",
+                    ADescription = DT.Rows[i]["ADescription"] + "",
+                    City = (int)DT.Rows[i]["City"],
+                    GuarantorID = (int)DT.Rows[i]["GuarantorID"],
+                    MinParticipants = (int)DT.Rows[i]["MinParticipants"],
+                    MaxParticipants = (int)DT.Rows[i]["MaxParticipants"],
+                    SpecialDemands = DT.Rows[i]["SpecialDemands"] + "",
+                    Category = (int)DT.Rows[i]["Category"]
+                });
+            }
+            DB.Close();
+            return LstActivitie;
+        }
+        public static Activitie GetById(int Id)
+        {
+            Activitie tmp = null;
+            string Sql = $"select * from T_Activitie where ActivitieID = {Id}";
+            DBContext DB = new DBContext();
+            DataTable DT = DB.Execute(Sql);
+            if (DT.Rows.Count > 0)
+            {
+                tmp = new Activitie()
+                {
+                    ActivitieID = (int)DT.Rows[0]["ActivitieID"],
+                    ActivityName = DT.Rows[0]["ActivityName"] + "",
+                    ADescription = DT.Rows[0]["ADescription"] + "",
+                    City = (int)DT.Rows[0]["City"],
+                    GuarantorID = (int)DT.Rows[0]["GuarantorID"],
+                    MinParticipants = (int)DT.Rows[0]["MinParticipants"],
+                    MaxParticipants = (int)DT.Rows[0]["MaxParticipants"],
+                    SpecialDemands = DT.Rows[0]["SpecialDemands"] + "",
+                    Category = (int)DT.Rows[0]["Category"]
+                };
+            }
+            DB.Close();
+            return tmp;
+        }
+        public static int DeleteById(int Id)
+        {
+            int RetVal = 0;
+            string Sql = $"delete * from T_Activitie Where ActivitieID={Id}";
+            DBContext Db = new DBContext();
+            RetVal = Db.ExecuteNonQuery(Sql);
+            Db.Close();
+            return RetVal;
+        }
     }
 }
--- Volunteer_Management_System/App_Code/DAL/AvailabilityDAL.cs
+++ Volunteer_Management_System/App_Code/DAL/AvailabilityDAL.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using BLL;
+using Data;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +10,81 @@ namespace DAL
 {
     public class AvailabilityDAL
     {
-        //
+        public static void Save(Availability tmp)
+        {
+
+            string Sql = "";
+            if (tmp.AvailabilityID == -1)
+            {
+                Sql = $"insert into T_Availability (UserID, numOfDay, BeginningTime, EndingTime)";
+                Sql += $" values (N'{tmp.UserID}',{tmp.numOfDay},{tmp.BeginningTime},{tmp.EndingTime} ";
+            }
+            else
+            {
+                Sql += "update T_Availability set ";
+                Sql += $" UserID= N'{tmp.UserID}' ";
+                Sql += $" numOfDay= N'{tmp.numOfDay}' ";
+                Sql += $" BeginningTime= N'{tmp.BeginningTime}' ";
+                Sql += $" EndingTime= N'{tmp.EndingTime}' ";
+                Sql += $" where AvailabilityID = {tmp.AvailabilityID} ";
+            }
+            DBContext DB = new DBContext();
+            DB.ExecuteNonQuery(Sql);
+            if (tmp.AvailabilityID == -1)
+            {
+                Sql = $"select max(AvailabilityID) from T_Availability where UserID= N'{tmp.UserID}' ";
+                tmp.AvailabilityID = (int)DB.ExecuteScalar(Sql);
+                DB.Close();
+            }
+        }
+        public static List<Availability> GetAll()
+        {
+            List<Availability> LstAvailability = new List<Availability>();
+            string Sql = "select * from T_Availability";
+            DBContext DB = new DBContext();
+            DataTable DT = DB.Execute(Sql);
+            for (int i = 0; i < DT.Rows.Count; i++)
+            {
+                LstAvailability.Add(new Availability()
+                {
+                    AvailabilityID = (int)DT.Rows[i]["AvailabilityID"],
+                    UserID = (int)DT.Rows[i]["UserID"],
+                    numOfDay = (int)DT.Rows[i]["numOfDay"],
+                    BeginningTime = (DateTime)DT.Rows[i]["BeginningTime"],
+                    EndingTime = (DateTime)DT.Rows[i]["EndingTime"],
+                });
+            }
+            DB.Close();
+            return LstAvailability;
+        }
+        public static Availability GetById(int Id)
+        {
+            Availability tmp = null;
+            string Sql = $"select * from T_Availability where AvailabilityID = {Id}";
+            DBContext DB = new DBContext();
+            DataTable DT = DB.Execute(Sql);
+            if (DT.Rows.Count > 0)
+            {
+                tmp = new Availability()
+                {
+                    AvailabilityID = (int)DT.Rows[0]["AvailabilityID"],
+                    UserID = (int)DT.Rows[0]["UserID"],
+                    numOfDay = (int)DT.Rows[0]["numOfDay"],
+                    BeginningTime = (DateTime)DT.Rows[0]["BeginningTime"],
+                    EndingTime = (DateTime)DT.Rows[0]["EndingTime"],
+                };
+            }
+            DB.Close();
+            return tmp;
+        }
+        public static int DeleteById(int Id)
+        {
+            int RetVal = 0;
+            string Sql = $"delete * from T_Availability Where AvailabilityID={Id}";
+            DBContext Db = new DBContext();
+            RetVal = Db.ExecuteNonQuery(Sql);
+            Db.Close();
+            return RetVal;
+        }
     }
 }
